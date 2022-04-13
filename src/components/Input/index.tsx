c4a2fa5431b62f66aa9bad2d46b1ffc3a8fd859e
LUCIO Notebook <luciononato@hotmail.com>
2022-04-13 04:29:39 -0300

    Modal and input components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,24 @@
 import {
+  InputHTMLAttributes,
   useEffect,
   useRef,
   useState,
   useCallback,
 } from 'react';
 
+import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement>{
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+
+const Input: React.FC<IInputProps> = ({name, icon: Icon, ...rest}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +56,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
 export default Input;
